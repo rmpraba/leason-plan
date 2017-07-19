755d71cdb0c40240fb8cabc96551314322e93631
mohamedabbas1992 <softabbas@gmail.com>
2017-07-19 10:17:28 -0700

    ALIGN

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var dbserver_ip_address = process.env.OPENSHIFT_MYSQL_DB_HOST || '127.0.0.1'
 var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
-   password : 'admin',
-   database : 'master1'
+   password : '',
+   database : 'reportcard1'
  });
 var bodyParser = require('body-parser'); 
 var app = express();
