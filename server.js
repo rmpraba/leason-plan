fa2637ea4f0df6b5f73f671e7876f0b87444468f
janani1995 <cutesuja24@gmail.com>
2017-07-22 19:18:41 +0530

    chapter

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : '',
-   database : 'reportcard11'
+   database : 'reportcard1'
  });
 var bodyParser = require('body-parser'); 
 var app = express();
